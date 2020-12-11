716e256128ccf39b1786d65b0783f298046b6f11
Sangeetha-Naresh <73550579+Sangeetha-Naresh@users.noreply.github.com>
2020-12-11 22:48:50 +0530

    Add files via upload

---
+++ js/Plinko.js
@@ -0,0 +1,33 @@
+class Plinko 
+{
+    constructor(x, y)
+     {
+        var options = 
+        {
+        
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        
+        World.add(world, this.body);
+    }
+    display() 
+    {
+
+        var pos = this.body.position;
+
+
+        push();
+        translate(pos.x, pos.y);
+    
+        imageMode(CENTER);
+    
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        
+        pop();
+    }
+
+};
